d25d383da989dfcd6efcad052de4697d29c21ea9
Camelia <Cameliaafolabi@gmail.com>
2025-02-24 12:19:06 +0100

    rectified deployment error

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import { generateId } from '@/lib/utils';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { IProduct } from '@/lib/db/models/product.model'
